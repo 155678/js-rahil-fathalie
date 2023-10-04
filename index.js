cd0e800c137b5f157284b2bfbf8e9a60a057d5a8
Maryam fathali nezhad shirazie <115879843+155678@users.noreply.github.com>
2023-10-04 12:09:06 +0330

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+let userFirstNumber = +prompt("please Enter FirstNumber!");
+console.log(userFirstNumber);
+console.log(typeof userFirstNumber);
+
+let userSecondsNumber = +prompt("please Enter SecodsNumber!");
+console.log(userSecondsNumber);
+console.log(typeof userSecondsNumber);
+
+
+let userSum = userFirstNumber + userSecondsNumber ;
+console.log(userSum);
+
+console.log(typeof userSum)
+
+document.write( userSum );
